583d9bf1ee14aa904bced0cd455aecb3fe9876a8
lexxai <lexxai@users.noreply.github.com>
2023-08-01 08:37:15 +0300

    limts

--- purge_old_chats.py
+++ purge_old_chats.py
@@ -9,20 +9,22 @@ def purge_old_messages():
     """ Select old chats messages that oldest than 24h and purge it """
     conn = None
 
-    LIMIT_ROW = 10
+    LIMIT_ROWS = 100
+    LIMIT_HOURS = 24
 
     try:
         # read connection parameters
         db_params = config()
         limits = config(section='limits')
-        LIMIT_ROW = limits.get('row')
+        LIMIT_ROWS = int(limits.get('rows'))
+        LIMIT_HOURS = int(limits.get('hours'))
     except (Exception) as error:
         print(error)
 
 
-
     SELECT_CHAT_REF = "SELECT DISTINCT object_id FROM public.chat_message_references"\
-                      " WHERE inserted_at <= NOW() - INTERVAL '24 HOURS' LIMIT {};".format(LIMIT_ROW)
+                      " WHERE inserted_at <= NOW() - INTERVAL '{0:d} HOURS' LIMIT {1:d};".format(
+                          LIMIT_HOURS, LIMIT_ROWS)
 
     COUNT_CHAT_OBJECTS = "SELECT count(*) FROM public.chat_message_references"\
                          " WHERE chat_id = %s;"   
@@ -36,14 +38,13 @@ def purge_old_messages():
     FIND_EMPTY_CHAT = "SELECT chats.id, chats.inserted_at  FROM chats"\
                       " LEFT JOIN chat_message_references ON chats.id = chat_message_references.chat_id"\
                       " WHERE chat_message_references.chat_id ISNULL"\
-                      " ORDER BY chats.inserted_at LIMIT {};".format(LIMIT_ROW)
+                      " ORDER BY chats.inserted_at LIMIT {};".format(LIMIT_ROWS)
 
 
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**db_params)
-
         #transaction start
         with conn:
             with conn.cursor() as cur:
@@ -51,11 +52,10 @@ def purge_old_messages():
                 # SELECT CHAT OBJECTS THAT OLDEST THAN 24h
                 cur.execute( SELECT_CHAT_REF )
                 rows_found = cur.rowcount
-                print(f"\nFOUND OLD CHAT MESSAGES: {rows_found}, with limit={LIMIT_ROW}")
+                print(f"\nFOUND OLD CHAT MESSAGES: {rows_found}, with limit={LIMIT_ROWS}")
                 rows = cur.fetchall()
                 for row in rows:
                     object_id = row[0]
-
                     # SELECT/DELETE OBJECTS BY ID
                     print(f"\nDELETE OBJECT: '{object_id}'")
                     #cur.execute( DELETE_OBJECT, (object_id,) )
@@ -63,27 +63,13 @@ def purge_old_messages():
                     cur.execute( SELECT_OBJECT, (object_id,) )
                     rows_deleted = cur.rowcount
                     if (rows_deleted):
-                        #o_row = cur.fetchone()
-                        #print("MUST DELETED: ",rows_deleted, o_row )
-
-                        #DELETE REF OBJECT
-                        #cur.execute(DELETE_CHAT_REF, object_id)
                         print("* DELETE REF OBJECT")
                         print(DELETE_CHAT_REF.replace('%s','{}').format(object_id))
-
-                        #COUNT CHAT_OBJECTS AFTER DELETING OLD
-                        # cur.execute( COUNT_CHAT_OBJECTS, (chat_id,) )
-                        # chat_objects_leaved = cur.fetchone()
-                        # print(f"chat_objects_leaved for {chat_id}: {chat_objects_leaved[0]}")
-
-                        # if chat_objects_leaved == 0:
-                        #     print("** DELETE CHAT")
-                        #     #cur.execute(DELETE_CHAT, chat_id)
-                        #     print(DELETE_CHAT.replace('%s','{}').format(chat_id))
+                #now find empty chats references on chats, end purge it
                 cur.execute( FIND_EMPTY_CHAT )
                 rows_found = cur.rowcount
                 if rows_found:
-                    print(f"\nEMPTY CHATS FOUND: {rows_found}, with limit={LIMIT_ROW}")
+                    print(f"\nEMPTY CHATS FOUND: {rows_found}, with limit={LIMIT_ROWS}")
                     rows = cur.fetchall()
                     for i,row in  enumerate(rows):
                         chat_id = row[0]
@@ -100,37 +86,5 @@ def purge_old_messages():
             conn.close()
             print('Database connection closed.')
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-
-        #transaction start
-        with conn:
-            with conn.cursor() as cur:
-            # execute a statement
-                print('PostgreSQL database version:')
-                cur.execute('SELECT version()')
-                # display the PostgreSQL database server version
-                db_version = cur.fetchone()
-                print(db_version)
-            # close the communication with the PostgreSQL
-                cur.close()
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
 if __name__ == '__main__':
-    #connect()
     purge_old_messages()
